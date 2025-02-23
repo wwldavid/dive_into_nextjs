ed7fed1574fec578ab5fe88193982afe358bcfd2
davidwanlongwu <2490050603@qq.com>
2025-02-22 23:17:51 -0700

    completed form validation and error handling

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -19,3 +19,44 @@ export async function editSnippet(id: number, code: string ){
 
       redirect('/snippets/'+ id)
 }
+
+export async function createSnippet(preState: {message: string},formData: FormData){
+    try{
+    const title = formData.get('title') as string
+    const code = formData.get('code') as string
+
+    if (typeof title !== 'string' || title.length < 3) {
+      return {
+        ...preState,
+        message:'Title must be longer than 3 characters'
+      }
+    }
+    if (typeof code !== 'string' || code.length < 3) {
+      return {
+        ...preState,
+        message:'Code must be longer than 3 characters'
+      }
+    }
+    
+    // throw new Error('😭')
+    
+    const snippet = await db.snippet.create({
+      data: {
+        title,
+        code
+      }
+    })
+  }catch(err){
+    if(err instanceof Error){
+      return {
+        message: err.message
+      }
+    } else {
+      return {
+        message:'Something went wrong!'
+      }
+    }
+  }
+    redirect('/')
+
+  }
--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,28 +1,23 @@
+'use client'
+import { createSnippet } from '@/actions'
 import { db } from '@/db'
 import { redirect } from 'next/navigation'
 import React from 'react'
+import { useFormState } from 'react-dom'
 
+
+const initState = {
+  message:'',
+}
 export default function page() {
-  async function createSnippet(formData: FormData){
-    'use server'
-    
-    const title = formData.get('title') as string
-    const code = formData.get('code') as string
-    
-    const snippet = await db.snippet.create({
-      data: {
-        title,
-        code
-      }
-    })
-    redirect('/')
 
-  }
+  const [ state, formAction ] = useFormState(createSnippet, initState)
+  
 
 
   return (
 
-    <form action={createSnippet}>
+    <form action={formAction}>
         <h3 className='font-bold m-3 text-center'>Create a Snippet</h3>
         <div className='flex flex-col gap-4'>
           {/* Title */}
@@ -35,7 +30,7 @@ export default function page() {
             <label className='w-12' htmlFor="title">Code</label>
             <input name='code' className='border border-teal-500 p-2 rounded w-full' type="text" id='title' />
           </div>
-
+           {state.message && <div className='my-2 p-2 bg-red-200 border rounded border-red-500' >{state.message}</div>}
           <button  className='rounded p-2 bg-blue-300' type='submit'>Create</button>
         </div>
     </form>
